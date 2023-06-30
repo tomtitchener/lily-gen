cef8c745c6bb0fc4429b161d1b3856705692daeb
ttitchener <ttitchener@gmail.com>
2023-06-30 17:32:03 -0400

    bug-fix

--- info.rkt
+++ info.rkt
@@ -1,3 +1,3 @@
 #lang info
 
-(define build-deps '("seq" "algorithms" "binary-class" "rack-check" "rackunit-lib"))
+(define build-deps '("seq" "algorithms" "binary-class" "rackcheck" "rackunit-lib"))
