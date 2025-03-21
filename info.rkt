721f5dc80240adcd94e97daae521ac65d75d74a3
ttitchener <ttitchener@gmail.com>
2025-03-21 11:42:06 -0400

    fix pkg deps

--- info.rkt
+++ info.rkt
@@ -1,3 +1,14 @@
 #lang info
 
-(define build-deps '("seq" "seq-lib" "algorithms" "simple-matrix" "rackcheck" "rackunit-lib"))
+(define build-deps '("at-exp-lib"
+                     "seq"
+                     "seq-lib"
+                     "algorithms"
+                     "simple-matrix"
+                     "rackcheck"
+                     "rackunit-lib"))
+(define deps '("algorithms"
+               "base"
+               "seq-lib"
+               "simple-matrix"
+               "srfi-lite-lib"))
